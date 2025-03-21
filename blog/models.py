3dc0e6f80e4eba24f6ea84ebf4ad3c6d41b3e74d
Dmitry <tdmitryny@gmail.com>
2025-03-20 20:25:30 -0400

    test launches

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
 from django.db import models
+from django.utils import timezone
+from tinymce.models import HTMLField
+from django.utils.text import slugify
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    title = models.CharField(max_length=200)
+    content = HTMLField()
+    created_at = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(max_length=200, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        ordering = ['-created_at']
+
+    
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    paginate_by = 10
